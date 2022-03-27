c6d8e1805c73b457c86442073de8097b353e3215
badger <badger@badger.local>
2022-03-27 20:34:36 +0300

    Basic react SSR

---
+++ server.js
@@ -0,0 +1,27 @@
+/*
+* npx babel-node server.js
+*/
+
+import express from "express";
+import React from "react";
+import {renderToString} from "react-dom/server";
+
+const app = express();
+
+app.get("/*", (req, res) => {
+	const reactApp = renderToString(
+		<h1>Hello from the server side</h1>
+	)
+
+	return res.send(`
+		<html>
+			<body>
+				<div id="root">${reactApp}</div>
+			</body>	
+		</html>
+	`)
+})
+
+app.listen(8080, () => {
+	console.log('server is listening on port 8080')
+})
